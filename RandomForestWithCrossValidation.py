107d82de8283fb9e6e88d7d66c45decb66e597bc
Shreyesh <imshreyesh17@gmail.com>
2020-08-06 19:11:28 -0500

    fixed the paths in RandomForestWithCrossValidation

--- RandomForestWithCrossValidation.py
+++ RandomForestWithCrossValidation.py
@@ -1,18 +1,12 @@
+import os
 import numpy as np
 import scipy as sp
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import recall_score
-from sklearn.metrics import accuracy_score 
-import pickle
-import joblib
-import itertools
-
+from sklearn.metrics import confusion_matrix, recall_score, accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
 
 # This part is irrelevant to the cross-validation
 pred_word=[]
@@ -26,10 +20,16 @@ def generate_words(arr_of_digraphs):
     for digraph_tuple in itertools.product(*arr_of_digraphs):
          pred_word.append(''.join(digraph_tuple))
 
-msu_dataset=pd.read_csv("msuupdated.csv")
-stony_dataset = pd.read_csv("stonybrooksdataset_updated.csv")
-greycweb_dataset = pd.read_csv("greycwebdata.csv")
-greyc_normal_dataset = pd.read_csv("greyc_normal.csv")
+dataPath = os.path.join(os.getcwd(),"Data")
+def _getDataFilePath(fileName):
+    return os.path.join(dataPath+os.sep, fileName)
+
+# Importing datasets
+msu_dataset=pd.read_csv(_getDataFilePath("msuupdated.csv"))
+stony_dataset = pd.read_csv(_getDataFilePath("stonybrooksdataset_updated.csv"))
+greycweb_dataset = pd.read_csv(_getDataFilePath("greycwebdata.csv"))
+greyc_normal_dataset = pd.read_csv(_getDataFilePath("greyc_normal.csv"))
+
 dataframe=pd.concat([greycweb_dataset, greyc_normal_dataset])
 
 X=dataframe.iloc[:,2:].values
